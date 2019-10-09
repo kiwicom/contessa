0b7c1436a4f259e0f10b880dc844e7f0cd18e3db
radka.meres <radka.meres@kiwi.com>
2019-10-09 12:22:20 +0200

    fix description is none

--- contessa/rules.py
+++ contessa/rules.py
@@ -81,7 +81,7 @@ class OneColumnRuleSQL(SqlRule):
     executor_cls = SqlExecutor
 
     def __init__(self, name, column, description, **kwargs):
-        if description == '' or description == None:
+        if description == '' or description is None:
             raise TypeError('Description is mandatory')
         super().__init__(name, description = description, **kwargs)
         self.column = column
