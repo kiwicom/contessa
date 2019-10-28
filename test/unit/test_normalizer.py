23d151b238b521ecc1da561572c48a0f9ded5abb
Jozef Cechovsky <jozef.cechovsky@kiwi.com>
2019-10-28 11:29:11 +0100

    repair tests

--- test/unit/test_normalizer.py
+++ test/unit/test_normalizer.py
@@ -11,37 +11,37 @@ from contessa.normalizer import RuleNormalizer
         (
             [
                 {
-                    "type": "n",
+                    "name": "not_null_name",
                     "type": "not_null",
                     "columns": ["a", "b", "c"],
                     "separate_time_filters": ["c", "u"],
                 }
             ],
             [
-                {"type": "n", "type": "not_null", "column": "a", "time_filter": "c"},
-                {"type": "n", "type": "not_null", "column": "b", "time_filter": "c"},
-                {"type": "n", "type": "not_null", "column": "c", "time_filter": "c"},
-                {"type": "n", "type": "not_null", "column": "a", "time_filter": "u"},
-                {"type": "n", "type": "not_null", "column": "b", "time_filter": "u"},
-                {"type": "n", "type": "not_null", "column": "c", "time_filter": "u"},
+                {"name": "not_null_name", "type": "not_null", "column": "a", "time_filter": "c"},
+                {"name": "not_null_name", "type": "not_null", "column": "b", "time_filter": "c"},
+                {"name": "not_null_name", "type": "not_null", "column": "c", "time_filter": "c"},
+                {"name": "not_null_name", "type": "not_null", "column": "a", "time_filter": "u"},
+                {"name": "not_null_name", "type": "not_null", "column": "b", "time_filter": "u"},
+                {"name": "not_null_name", "type": "not_null", "column": "c", "time_filter": "u"},
             ],
         ),
         (
-            [{"type": "n", "type": "not_null", "columns": ["a", "b"]}],
+            [{"name": "not_null_name", "type": "not_null", "columns": ["a", "b"]}],
             [
-                {"type": "n", "type": "not_null", "column": "a", "time_filter": None},
-                {"type": "n", "type": "not_null", "column": "b", "time_filter": None},
+                {"name": "not_null_name", "type": "not_null", "column": "a", "time_filter": None},
+                {"name": "not_null_name", "type": "not_null", "column": "b", "time_filter": None},
             ],
         ),
         (
-            [{"type": "n", "type": "not_null", "column": "a", "time_filter": "a"}],
-            [{"type": "n", "type": "not_null", "column": "a", "time_filter": "a"}],
+            [{"name": "not_null_name", "type": "not_null", "column": "a", "time_filter": "a"}],
+            [{"name": "not_null_name", "type": "not_null", "column": "a", "time_filter": "a"}],
         ),
     ],
 )
 def test_normalizer(rules_def, normalized):
     results = RuleNormalizer().normalize(rules_def)
-    op = operator.itemgetter("type", "type", "column", "time_filter")
+    op = operator.itemgetter("name", "type", "column", "time_filter")
     expected = sorted(normalized, key=op)
     results_sorted = sorted(results, key=op)
     assert expected == results_sorted
