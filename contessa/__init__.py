69e9ba8c6f73c2c590df8fa7457e7f59d5dcb2ed
Dominik Gmiterko <dominik.gmiterko@kiwi.com>
2021-06-25 08:44:26 +0200

    Release 0.2.12

--- contessa/__init__.py
+++ contessa/__init__.py
@@ -6,7 +6,7 @@ It exposes `DataQualityOperator` that can be plugged in any airflow DAG and will
 check on load in temporary table.
 """
 
-__version__ = "0.2.11"
+__version__ = "0.2.12"
 
 # Start ignoring PyUnusedCodeBear
 from .consistency_checker import ConsistencyChecker
